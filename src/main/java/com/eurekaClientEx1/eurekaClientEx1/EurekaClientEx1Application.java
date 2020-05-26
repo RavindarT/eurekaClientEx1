ddc064c6201033197a1bca04df19c08f908b5fba
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-26 11:15:48 -0400

    commit

---
+++ src/main/java/com/eurekaClientEx1/eurekaClientEx1/EurekaClientEx1Application.java
@@ -0,0 +1,13 @@
+package com.eurekaClientEx1.eurekaClientEx1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientEx1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurekaClientEx1/eurekaClientEx1/EurekaClientEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaClientEx1.eurekaClientEx1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
